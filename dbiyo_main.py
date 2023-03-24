a13cea1cb2abd5e98e62953b9c2f6f69decb3938
Julian <jullian_1999@outlook.com>
2023-03-24 17:48:31 +0100

    add dbiyo file

--- dbiyo_main.py
+++ dbiyo_main.py
@@ -1,15 +1,14 @@
 import math
-
 import cv2 as cv2
 import numpy as np
 
 
-COLUMNS = 9
+COLUMNS = 5
 ROWS = 30
 ANSWERS = 3
 
 epsilon = 10 #image error sensitivity
-filename = "./scanned/sanitycheck.jpg"
+filename = "./scanned/form11.jpg"
 
 # load tracking tags
 tags = [cv2.imread("markers/top_left.png", cv2.IMREAD_GRAYSCALE),
@@ -18,10 +17,10 @@ tags = [cv2.imread("markers/top_left.png", cv2.IMREAD_GRAYSCALE),
         cv2.imread("markers/bottom_right.png", cv2.IMREAD_GRAYSCALE)]
 
 scaling = [869.0, 840.0] #scaling factor for 8.5in. x 11in. paper
-columns = [[55.5 / scaling[0], 66.5 / scaling[1]]] #dimensions of the columns of bubbles
-colspace = 76.8 /scaling[0]
-radius = 6.5 / scaling[0] #radius of the bubbles
-spacing = [24.9 / scaling[0], 19.9 / scaling[1]] #spacing of the rows and columns
+columns = [[83.0 / scaling[0], 42.5 / scaling[1]]] #dimensions of the columns of bubbles
+colspace = 135.8 /scaling[0]
+radius = 8.5 / scaling[0] #radius of the bubbles
+spacing = [35.2 / scaling[0], 22.25 / scaling[1]] #spacing of the rows and columns
 
 # Load the image from file
 img = cv2.imread(filename)
@@ -47,6 +46,7 @@ def FindCorners(paper, drawRect):
 
         #convolve the image
         convimg = (cv2.filter2D(np.float32(cv2.bitwise_not(gray_paper)), -1, np.float32(cv2.bitwise_not(tag))))
+
 
         #find the maximum of the convolution
         corner = np.unravel_index(convimg.argmax(), convimg.shape)
@@ -75,17 +75,16 @@ def FindCorners(paper, drawRect):
 
     return
 
-FindCorners(img, False)
-print(corners)
-
-desired_points = np.float32([[68, 424], [1489, 424], [68, 1797], [1489, 1797]])
-points = np.float32(corners)
-
-M = cv2.getPerspectiveTransform(points, desired_points)
-sheet = cv2.warpPerspective(img, M, (1589, 1997))
-
-
-img = sheet
+# FindCorners(img, False)
+# print(corners)
+#
+# desired_points = np.float32([[68, 424], [1489, 424], [68, 1797], [1489, 1797]])
+# points = np.float32(corners)
+#
+# M = cv2.getPerspectiveTransform(points, desired_points)
+# sheet = cv2.warpPerspective(img, M, (1589, 1997))
+#
+# img = sheet
 height, width, channels = img.shape
 corners = []
 FindCorners(img, True)
@@ -97,8 +96,8 @@ treshImg, thresh = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY_INV)
 
 xdis, ydis = corners[3][0] - corners[0][0], corners[3][1] - corners[0][1]
 answersBoundingbox = [(int(corners[0][0] + 0.035 *xdis), corners[0][1] + int(0.055 *ydis)), (corners[3][0] - int(0.15 * xdis), corners[3][1] - int(0.21 * ydis))]
-cv2.rectangle(img, answersBoundingbox[0],
-        answersBoundingbox[1], (0, 255, 0), thickness=2, lineType=8, shift=0)
+# cv2.rectangle(img, answersBoundingbox[0],
+#         answersBoundingbox[1], (0, 255, 0), thickness=2, lineType=8, shift=0)
 
 # calculate dimensions for scaling
 dimensions = [corners[1][0] - corners[0][0], corners[2][1] - corners[0][1]]
@@ -124,25 +123,25 @@ for i in range(0, ROWS):  # rows
             roi = thresh[y1:y2, x1:x2]
 
             percentile = (np.sum(roi == 255)/((y2-y1) * (x2-x1))) * 100
-            print(percentile)
+            # print(percentile)
 
             if percentile > 40.0:
                 if (j != 0 and boulders[i][j] == 0) or j == 0:
-                    boulders[i][j] = k + 1
+                    boulders[i][j] = k+1
 
 for i in range(0, ROWS):
-    x1 = int((columns[0][0] + colspace * 9.7) * dimensions[0] + corners[0][0])
+    x1 = int((columns[0][0] + colspace * 5.1) * dimensions[0] + corners[0][0])
     y1 = int((columns[0][1]+0.005 + i * spacing[1]) * dimensions[1] + corners[0][1])
     cv2.putText(img, str(boulders[i][1]), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 190, 0), 2)
 
-    x2 = int((columns[0][0] + colspace * 10.5) * dimensions[0] + corners[0][0])
+    x2 = int((columns[0][0] + colspace * 5.7) * dimensions[0] + corners[0][0])
     cv2.putText(img, str(boulders[i][2]), (x2, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 190, 0), 2)
 
 
 print(boulders)
 # Show the image with the rectangles
 cv2.imwrite(f"checkboxes_{filename}", img)
-cv2.imshow("Checkboxes", cv2.resize(img, (0, 0), fx=0.7, fy=0.7))
+cv2.imshow("Checkboxes", cv2.resize(img, (0, 0), fx=0.5, fy=0.5))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
